52d5e2c1bd5722f42d91ce785a55cb3805050334
Stuart P. Bentley <s@stuartpb.com>
2018-01-21 05:26:28 -0800

    Unify Windows and non-Windows behavior

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
-const isWin = process.platform === 'win32';
-
 module.exports = function (isSoft) {
 	process.stdout.write(
-		(isWin || isSoft) ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H'
+		isSoft ? '\x1Bc' : '\x1B[2J\x1B[3J\x1B[H\x1Bc'
 	);
 }
