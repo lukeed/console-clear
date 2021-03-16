3b809f5647b2a246bc95aff28d6283466505a472
milahu <milahu@gmail.com>
2021-03-16 08:44:04 +0100

    keep all the history

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-module.exports = function (isSoft) {
+module.exports = function (keepHistory) {
 	process.stdout.write(
-		isSoft ? '\x1B[H\x1B[2J' : '\x1B[2J\x1B[3J\x1B[H\x1Bc'
+		keepHistory
+			? ('\n'.repeat(process.stdout.rows) + '\x1B[H\x1B[2J')
+			: '\x1B[2J\x1B[3J\x1B[H\x1Bc'
 	);
 }
